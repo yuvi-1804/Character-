7aefb2e8d8509b87eafe38dc924a7614393e8dc0
yuvi-1804 <58116768+yuvi-1804@users.noreply.github.com>
2021-11-24 16:36:52 +0530

    Add files via upload

---
+++ char1.java
@@ -0,0 +1,12 @@
+class char1 {
+    char degree = 'B';
+    char alphabet='\u0042';
+    void compute(){
+        System.out.println(degree);
+        System.out.println(alphabet);
+    }
+public static void main(String args[]) {
+    char1 c =new char1();
+    c.compute();
+}
+}
